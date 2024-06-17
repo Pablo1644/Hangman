c7881aaf0db512a0b46a51fa9d5ff6bf0017893f
Pablo1644 <62251949+Pablo1644@users.noreply.github.com>
2024-06-17 12:16:16 -0100

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -4,16 +4,14 @@ import random
 
 # Initialize Pygame
 pygame.init()
-
-# Opening our file
-my_file = open("words.txt", "r",encoding="utf-8") # polish signs
+my_file = open("words.txt", "r", encoding="utf-8")
 data = my_file.read()
 words = data.split("\n")
-word = random.choice(words) #  Random word
-pygame.display.set_caption('Wisielec') # Title
-# VARIABLES
-checked_letters = []    # list for input letters
+word = random.choice(words)
 
+pygame.display.set_caption('Wisielec')  # Title
+# VARIABLES
+checked_letters = []
 
 
 class Game:
@@ -25,81 +23,76 @@ class Game:
         self.screen.fill((100, 121, 12))
         cords = (100, 200)
         length = int(300 / len(word))
-        for w in word:
+        for _ in word:
             pygame.draw.line(self.screen, (0, 0, 0), cords, (cords[0] + length, cords[1]))
             cords = (cords[0] + 2 * length, cords[1])
         pygame.display.flip()
-    
-    # printing lines - each line for each char
-    def print_lines(self, word, letter):
+
+    def print_lines(self, my_word, letter):
         cords = (100, 200)
-        length = int(300 / len(word))
-        for w in word:
+        length = int(300 / len(my_word))
+        for w in my_word:
 
             if w == letter:
                 self.draw_text(w, pygame.font.Font(None, 36), (255, 255, 255), cords[0] + length // 2, cords[1] - 20)
             cords = (cords[0] + 2 * length, cords[1])
         pygame.display.flip()
-    
-    # printing input letters
+
     def print_checked_letters(self):
         x_start = 50
         y = 50
         for idx, letter in enumerate(checked_letters):
             x = x_start + idx * 30
             self.draw_text(letter, pygame.font.Font(None, 40), (255, 255, 255), x, y)
-    
-    # printing proper chars for our word
+
     def draw_text(self, text, font, color, x, y):
         text_surface = font.render(text, True, color)
         text_rect = text_surface.get_rect()
         text_rect.center = (x, y)
         self.screen.blit(text_surface, text_rect)
-    
-    # drawing hangman
+
     def draw_hangman(self):
         cord = (500, 550)
         length = 90
         width = 4
-        match self.lifes: # after every mistake life is decreased.
+        match self.lifes:
             case 11:
-                pygame.draw.line(self.screen, (0, 0, 0), cord, (cord[0] + length, cord[1]),width)
+                pygame.draw.line(self.screen, (0, 0, 0), cord, (cord[0] + length, cord[1]), width)
             case 10:
-                cord = (cord[0]+length/2, cord[1])
-                pygame.draw.line(self.screen, (0, 0, 0), cord, (cord[0], cord[1]-2*length),width)
+                cord = (cord[0] + length / 2, cord[1])
+                pygame.draw.line(self.screen, (0, 0, 0), cord, (cord[0], cord[1] - 2 * length), width)
             case 9:
-                cord = (cord[0]+length/2, cord[1]-2*length)
-                pygame.draw.line(self.screen, (0, 0, 0), cord, (cord[0]+length, cord[1]),width)
+                cord = (cord[0] + length / 2, cord[1] - 2 * length)
+                pygame.draw.line(self.screen, (0, 0, 0), cord, (cord[0] + length, cord[1]), width)
             case 8:
-                cord = (cord[0]+length,cord[1]-2*length)
-                pygame.draw.line(self.screen, (0, 0, 0), cord, (cord[0]-length/2, cord[1]+length/2),width)
+                cord = (cord[0] + length, cord[1] - 2 * length)
+                pygame.draw.line(self.screen, (0, 0, 0), cord, (cord[0] - length / 2, cord[1] + length / 2), width)
             case 7:
-                cord = (cord[0]+1.5*length,cord[1]-2*length)
+                cord = (cord[0] + 1.5 * length, cord[1] - 2 * length)
                 pygame.draw.line(self.screen, (0, 0, 0), cord, (cord[0], cord[1] + length / 2), width)
             case 6:
-                cord = (cord[0]+1.5*length,cord[1]-1.25*length)
-                pygame.draw.circle(self.screen, (0, 0, 0), cord, length/4,width)
+                cord = (cord[0] + 1.5 * length, cord[1] - 1.25 * length)
+                pygame.draw.circle(self.screen, (0, 0, 0), cord, length / 4, width)
             case 5:
-                cord = (cord[0] + 1.5 * length, cord[1] - length - width/2)
-                pygame.draw.line(self.screen, (0, 0, 0), cord, (cord[0]-length/8, cord[1]+length/4), width)
+                cord = (cord[0] + 1.5 * length, cord[1] - length - width / 2)
+                pygame.draw.line(self.screen, (0, 0, 0), cord, (cord[0] - length / 8, cord[1] + length / 4), width)
             case 4:
-                cord = (cord[0] + 1.5 * length, cord[1] - length - width/2)
+                cord = (cord[0] + 1.5 * length, cord[1] - length - width / 2)
                 pygame.draw.line(self.screen, (0, 0, 0), cord, (cord[0] + length / 8, cord[1] + length / 4), width)
             case 3:
                 cord = (cord[0] + 1.5 * length, cord[1] - length - width / 2)
                 pygame.draw.line(self.screen, (0, 0, 0), cord, (cord[0], cord[1] + length / 2), width)
             case 2:
-                cord = (cord[0] + 1.5 * length, cord[1] - length/2 - width / 2)
+                cord = (cord[0] + 1.5 * length, cord[1] - length / 2 - width / 2)
                 pygame.draw.line(self.screen, (0, 0, 0), cord, (cord[0] - length / 8, cord[1] + length / 4), width)
             case 1:
                 cord = (cord[0] + 1.5 * length, cord[1] - length / 2 - width / 2)
                 pygame.draw.line(self.screen, (0, 0, 0), cord, (cord[0] + length / 8, cord[1] + length / 4), width)
                 return False
         self.lifes -= 1
-    
-    # main function 
-    def make_game(self, word):
-        counter = word.__len__()
+
+    def make_game(self, my_word):
+        counter = my_word.__len__()
         running = True
         while running:
             # Handle user input and events
@@ -109,26 +102,28 @@ class Game:
                 elif event.type == pygame.KEYDOWN:
                     if event.unicode.isalpha():  # Check if the pressed key is a letter
                         letter = event.unicode  # Update the letter variable with the pressed letter
-                        if letter not in word:
+                        if letter not in my_word:
                             if letter not in checked_letters:
                                 checked_letters.append(letter)
                                 print(checked_letters)
-                                if self.draw_hangman()==False:
+                                if not self.draw_hangman():
                                     running = False
                         else:
                             if letter not in checked_letters:
                                 checked_letters.append(letter)
-                                decrement = word.count(letter) # case if there is more than one correct letter in word - for example in "ananas",there is 3 "a".
-                                counter-=decrement
-                        self.print_lines(word, letter)
+                                decrement = my_word.count(letter)
+                                print(checked_letters)
+                                counter -= decrement
+                        self.print_lines(my_word, letter)
                 if counter == 0:
                     running = False
                 self.print_checked_letters()
                 pygame.display.flip()
 
+        print(my_word)
         pygame.quit()
-        sys.exit() 
+        sys.exit()
 
 
-scr = Game() # object from class Game
-scr.make_game(word) 
+scr = Game()
+scr.make_game(word)
